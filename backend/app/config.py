77d2a8023219932e532821f638df32ff17980327
Boris <boris2000n@gmail.com>
2020-03-22 22:35:46 +0300

    docker auto

--- backend/app/config.py
+++ backend/app/config.py
@@ -22,7 +22,7 @@ try:
 except:
     logger.exception('Fail to get environment variables for db connection')
 
-VERSION = '2.4'
+VERSION = '2.4.1'
 API_SERVER_REQUEST_VERSION = 0
 
 
--- backend/upload.sh
+++ backend/upload.sh
@@ -7,7 +7,8 @@ git add .
 git commit -m"docker auto"
 git push origin docker
 
-ssh root@95.182.122.120 cd /app/NTrail && git pull && git checkout docker && docker-compose down && docker-compose build --no-cache && docker-compose start && exit
+ssh root@95.182.122.120 cd /app/NTrail && git pull 
+#&& git checkout docker && docker-compose down && docker-compose build --no-cache && docker-compose start && exit
 
 echo Done upload!
 
