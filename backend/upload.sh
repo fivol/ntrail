509b7570944d607b5bdd15c526e6104a2eb263c1
Boris <boris2000n@gmail.com>
2020-03-22 22:55:54 +0300

    docker auto

--- backend/upload.sh
+++ backend/upload.sh
@@ -8,7 +8,7 @@ git commit -m"docker auto"
 git push origin docker
 
 #ssh root@95.182.122.120 "/app/NTrail/pull.sh"
-ssh root@95.182.122.120 "cd /app/NTrail && git pull"
+ssh root@95.182.122.120 "cd /app/NTrail && git pull && docker-compose build --no-cache && docker-compose down && docker-compose start"
 
 echo Done upload!
 
