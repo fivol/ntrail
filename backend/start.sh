8c90794a2bc196c9b94b689b6ed1c9bf2f8be2e4
boris <borisoffficial@gmail.com>
2021-02-25 20:05:43 +0300

    fix update

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,7 @@
 
 sudo apt update -y &&
 sudo apt upgrade -y &&
-sudo apt install apt-transport-https ca-certificates curl software-properties-common &&
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common &&
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" &&
 sudo apt update -y &&
--- backend/update.sh
+++ backend/update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-git pull
-docker-compose stop
-docker-compose build
-docker-compose up -d
+git pull &&
+docker-compose stop &&
+docker-compose build &&
+docker-compose up -d &&
 
 echo App updated successfully!
 
