8e571433b9f150d1cc7826f9b0dc573f0e7192f6
boris <borisoffficial@gmail.com>
2021-02-25 20:10:06 +0300

    new build num

--- backend/app/config.py
+++ backend/app/config.py
@@ -22,7 +22,7 @@ try:
 except:
     logger.exception('Fail to get environment variables for db connection')
 
-VERSION = '2.11.1'
+VERSION = '2.11.2'
 API_SERVER_REQUEST_VERSION = 0
 
 # Способ кеширования
