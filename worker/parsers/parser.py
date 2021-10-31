88402bc9a6e549feec276302131b1895feadd309
Boris Bondarenko <borisoffficial@gmail.com>
2021-10-31 19:26:51 +0300

    rm celery

--- worker/parsers/parser.py
+++ worker/parsers/parser.py
@@ -1,9 +1,6 @@
-from celery_wrapper.task_executing import TasksExecutorMeta
-from session.exceptions import SessionException
-from celery import Task
 
 
-class BaseParser(metaclass=TasksExecutorMeta):
+class BaseParser:
     """
     Базовый класс для парсеров.
     Парсеры это просто группа методов взаимодейсвия с одним сервисом,
@@ -12,13 +9,3 @@ class BaseParser(metaclass=TasksExecutorMeta):
     Парсер может реализовывать высокоуровневые методы, которые исполняются
     посредством создания ряда дополнительных задач
     """
-    @classmethod
-    async def _wrapper(cls, method, args, kwargs, *_, task: Task = None, **__):
-        """
-        Calling every time when call any method of child classes
-        """
-        try:
-            return await method(*args, **kwargs)
-        except SessionException:
-            # Не удалось выполнить задачу из-за проблем с сессией, первый запрос провалился
-            return task.replace(task.signature(args, kwargs))
