cf2ae966c7760704e4fbf9a88fcae59a9b740a71
Boris Bondarenko <borisoffficial@gmail.com>
2021-11-01 16:39:17 +0300

    refactoring and decomposition

--- worker/worker/parsers/vk/vk.py
+++ worker/worker/parsers/vk/vk.py
@@ -9,8 +9,7 @@ from worker.helpers.caching import redis_cache
 from worker.parsers.parser import BaseParser
 from worker.parsers.vk.data import *
 from worker.parsers.vk.exceptions import VKError
-from worker.parsers.vk.layers import partition_split, count_offset_iterator, items_getter, ListWithCount, make_synced, \
-    reliable_call
+from worker.parsers.vk.layers import *
 from worker.session.exceptions import SessionManagerException
 from worker.session.session_manager import SessionManager
 from worker.session.session_state import SessionState
@@ -56,7 +55,83 @@ EXECUTE_MAX_LENGTH = 12000
 
 
 class ExecuteRequestPool:
-    pass
+    """
+    https://vk.com/dev/execute
+    """
+
+    def __init__(self):
+        self._execute_epoch = 0
+        self._execute_results = {}
+        self._executable_pool = []
+        self._execute_length = 0
+
+    @staticmethod
+    def _gen_execute_code(items) -> str:
+        """Item be like
+        [('users.get', {'user_id': 123})]
+        """
+        commands = [
+            f'API.{method_name}({json.dumps(kwargs, separators=(",", ":"))})'
+            for method_name, kwargs in items
+        ]
+        return f'return [{",".join(commands)}];'
+
+    async def _run_execute_pool(self, only_user_access=False):
+        # Label results as waiting
+        if not self._executable_pool:
+            raise IndexError
+        logger.debug('Run execute pool %s', len(self._executable_pool))
+        code = self._gen_execute_code(self._executable_pool)
+        execute_coro = VkMethods.execute(code, only_user_access=only_user_access)
+        execute_task = asyncio.create_task(execute_coro)
+
+        self._execute_results[self._execute_epoch] = execute_task
+        self._execute_epoch += 1
+        self._executable_pool = []
+        self._execute_length = 0
+
+    async def try_use_execute(self, method, kwargs, only_user_access=False):
+        """Tries to add request to execute pool, if success returns result, else None"""
+        cmd_idx = len(self._executable_pool)
+        curr_epoch = self._execute_epoch
+        cmd_length = len(json.dumps((method, kwargs)))
+
+        if cmd_length > EXECUTE_MAX_LENGTH:
+            logger.warning('Too long command to use execute command: %s', cmd_length)
+            return None
+
+        self._executable_pool.append((method, kwargs))
+        self._execute_length += cmd_length
+        if len(self._executable_pool) == EXECUTE_QUERIES_BUNCH_COUNT or \
+                self._execute_length + cmd_length > EXECUTE_MAX_LENGTH:
+            await self._run_execute_pool(only_user_access)
+
+        # Very important. We should return control to collect many queries in pool in async
+        await asyncio.sleep(0)
+        if len(self._executable_pool) < 15:
+            await asyncio.sleep(0.001)
+        if self._execute_epoch == curr_epoch and len(self._executable_pool) >= 3:
+            await self._run_execute_pool(only_user_access)
+        results = self._execute_results.get(curr_epoch, None)
+
+        if results is None:
+            self._executable_pool = []
+            return None
+
+        if isinstance(results, asyncio.Task):
+            real_results = await results
+            self._execute_results[curr_epoch] = [real_results, len(real_results)]
+
+        results, remain_count = self._execute_results[curr_epoch]
+        assert isinstance(results, list)
+        if results[0] is False:
+            logger.warning('Execute query returns False')
+            raise Warning('It seems, execute query failed')
+        self._execute_results[curr_epoch][1] -= 1
+        result = results[cmd_idx]
+        if not self._execute_results[curr_epoch][1]:
+            del self._execute_results[curr_epoch]
+        return result
 
 
 class VkMethods(BaseParser):
@@ -75,10 +150,7 @@ class VkMethods(BaseParser):
     # # 5 in docs
     _app_api = SessionManager(key_type='vk.app.token', controller=VkApiSession, max_rps=5)
 
-    _execute_epoch = 0
-    _execute_results = {}
-    _executable_pool = []
-    _execute_length = 0
+    _execute_pool = ExecuteRequestPool()
 
     @classmethod
     async def stop(cls):
@@ -90,30 +162,14 @@ class VkMethods(BaseParser):
     # To call any available api for example
 
     @classmethod
-    def _gen_execute_code(cls, items) -> str:
-        """Item be like
-        [('users.get', {'user_id': 123})]
-        """
-        commands = [
-            f'API.{method_name}({json.dumps(kwargs, separators=(",", ":"))})'
-            for method_name, kwargs in items
-        ]
-        return f'return [{",".join(commands)}];'
-
-    @classmethod
-    async def _run_execute_pool(cls, only_user_access=False):
-        # Label results as waiting
-        if not cls._executable_pool:
-            raise IndexError
-        logger.debug('Run execute pool %s', len(cls._executable_pool))
-
-        execute_coro = cls.execute(cls._gen_execute_code(cls._executable_pool), only_user_access=only_user_access)
-        execute_task = asyncio.create_task(execute_coro)
-
-        cls._execute_results[cls._execute_epoch] = execute_task
-        cls._execute_epoch += 1
-        cls._executable_pool = []
-        cls._execute_length = 0
+    async def _call_api(cls, method, kwargs, apis):
+        for i, api in enumerate(apis):
+            try:
+                with api.get() as session:
+                    return await session(method, **kwargs)
+            except SessionManagerException:
+                if i == len(apis) - 1:
+                    raise
 
     @classmethod
     async def _run_query(cls, method, kwargs, apis, executable=False, only_user_access=False, **other):
@@ -122,66 +178,21 @@ class VkMethods(BaseParser):
         Tried to group queries to execute it in single vk api execute commands
         If not enough commands, do is without execute, just with calling api
         """
+
         def handle_value(value):
             if isinstance(value, list):
                 return ','.join(map(str, value))
             return value
+
         kwargs = {key: handle_value(value) for key, value in kwargs.items() if value}
 
-        async def run_with_api():
-            for i, api in enumerate(apis):
-                try:
-                    with api.get() as session:
-                        return await session(method, **kwargs)
-                except SessionManagerException:
-                    if i == len(apis) - 1:
-                        raise
-
         if not executable:
-            return await run_with_api()
+            return await cls._call_api(method, kwargs, apis)
 
-        # Only for executable commands
-
-        cmd_idx = len(cls._executable_pool)
-        curr_epoch = cls._execute_epoch
-        cmd_length = len(json.dumps((method, kwargs)))
-
-        if cmd_length > EXECUTE_MAX_LENGTH:
-            logger.warning('Too long command to use execute command: %s', cmd_length)
-            return await run_with_api()
-
-        cls._executable_pool.append((method, kwargs))
-        cls._execute_length += cmd_length
-        if len(cls._executable_pool) == EXECUTE_QUERIES_BUNCH_COUNT or \
-                cls._execute_length + cmd_length > EXECUTE_MAX_LENGTH:
-            await cls._run_execute_pool(only_user_access)
-
-        # Very important. We should return control to collect many queries in pool in async
-        await asyncio.sleep(0)
-        if len(cls._executable_pool) < 15:
-            await asyncio.sleep(0.001)
-        if cls._execute_epoch == curr_epoch and len(cls._executable_pool) >= 3:
-            await cls._run_execute_pool(only_user_access)
-        results = cls._execute_results.get(curr_epoch, None)
-
-        if results is None:
-            cls._executable_pool = []
-            return await run_with_api()
-
-        if isinstance(results, asyncio.Task):
-            real_results = await results
-            cls._execute_results[curr_epoch] = [real_results, len(real_results)]
-
-        results, remain_count = cls._execute_results[curr_epoch]
-        assert isinstance(results, list)
-        if results[0] is False:
-            logger.warning('Execute query returns False')
-            raise Warning('It seems, execute query failed')
-        cls._execute_results[curr_epoch][1] -= 1
-        result = results[cmd_idx]
-        if not cls._execute_results[curr_epoch][1]:
-            del cls._execute_results[curr_epoch]
-        return result
+        execute_result = await cls._execute_pool.try_use_execute(method, kwargs, only_user_access)
+        if execute_result is None:
+            return await cls._call_api(method, kwargs, apis)
+        return execute_result
 
     @classmethod
     @decorate(reliable_call, partition_split(1000), redis_cache, make_synced)
