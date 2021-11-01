546009128809de492c25e5c9eb7ed3c3968d7615
Boris Bondarenko <borisoffficial@gmail.com>
2021-11-01 23:05:59 +0300

    fixes

--- worker/worker/parsers/vk/exceptions.py
+++ worker/worker/parsers/vk/exceptions.py
@@ -1,6 +1,17 @@
+from enum import Enum
+
 from aiovk.exceptions import VkAPIError
 
 from worker.parsers.exceptions import ParserRealError
+
+
+class VKErrorType(Enum):
+    UNKNOWN = -1
+
+    PRIVATE_PROFILE = 30
+    USER_WAS_BANNED = 37
+    UNKNOWN_USER = 39
+    UNKNOWN_GROUP = 40
 
 
 class VKError(ParserRealError):
@@ -15,6 +26,10 @@ class VKError(ParserRealError):
     @property
     def msg(self):
         return self._error.error_msg
+
+    @property
+    def type(self):
+        return VKErrorType(self.code)
 
     def __str__(self):
         return f'VK API Error: ({self.code}) {self.msg}'
