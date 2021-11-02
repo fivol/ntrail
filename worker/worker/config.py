cb8cf0bc328c042f75e71aa4221a26ed597f7e10
Boris Bondarenko <borisoffficial@gmail.com>
2021-11-02 11:52:51 +0300

    config refactoring and interface changes

--- worker/worker/config.py
+++ worker/worker/config.py
@@ -1,13 +1,12 @@
 import logging.config
 from bestconfig import Config
 
-logger = logging.getLogger(__name__)
-logging_config = Config('logging.yml', exclude_default=True)
-logging.config.dictConfig(logging_config.to_dict())
-
 
 config = Config()
 assert config.contains('DEBUG'), 'Probably you miss specify config.yml file with necessary variables'
+
+logger = logging.getLogger(__name__)
+logging.config.dictConfig(config.logging.to_dict())
 
 if not config.bool('DEBUG'):
     logging.disable(logging.WARNING)
