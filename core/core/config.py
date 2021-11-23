816ad95a563a8b0e9c5e21f3660c430eabb279c6
Boris Bondarenko <borisoffficial@gmail.com>
2021-11-24 02:10:47 +0300

    Fix config

--- core/core/config.py
+++ core/core/config.py
@@ -1,21 +1,11 @@
-import os
 import warnings
 import logging.config
 from bestconfig import Config
 
-from core.constants import CacheType
 
-PLOT_CIRCULAR = 'circular'
 warnings.simplefilter("ignore")
 config = Config()
 
-
-# Способ кеширования
-# CACHE_TYPE_FULL_USE - максимальное возможное
-# TODO Add other ways to cache requests description
-CACHE_TYPE = CacheType.FULL_USE
-
-if not os.environ.get('LOGGING_CONFIGURED'):
-    logging.config.dictConfig(config.logging.to_dict())
-    logger = logging.getLogger(__name__)
+logging.config.dictConfig(config.logging.to_dict())
+logger = logging.getLogger(__name__)
 
--- core/main.py
+++ core/main.py
@@ -1,27 +1,15 @@
+import asyncio
 import warnings
 import core.config # noqa
-warnings.simplefilter('ignore')
-
-from core.modules import VKUser
 from worker import Engine
 
-my_id = 245089915
-
-# private profiles
-# 431956287
-# 444845865
-# 455899120
-# 457559807
-# 460844711
-# 537901892
-# 558287310
-# 608492386
+warnings.simplefilter('ignore')
 
 
-def main():
-    VKUser('ffboris').friends().export('.data/friends-friends.csv')
+async def main():
+    async with Engine(caching=True):
+        pass
 
 
 if __name__ == '__main__':
-    with Engine(caching=True, timeout=10):
-        main()
+    asyncio.run(main())
