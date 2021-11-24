d5a9b7fa34642b093651a7f00210f3691a5c4524
Boris Bondarenko <borisoffficial@gmail.com>
2021-11-25 00:51:15 +0300

    More accurate instagram search

--- api/server/routes/vk/plugins/instagram.py
+++ api/server/routes/vk/plugins/instagram.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 import typing
-from collections import Counter
+from collections import Counter, defaultdict
 
 from core import VKUser, IGUser, IGCommunity
 from server.plugin.plugin import BasePlugin
@@ -28,35 +28,59 @@ class FindInstagramPlugin(BasePlugin):
             # TODO Process inst exception
             return None
 
-    async def response(self) -> typing.Optional[str]:
+    @classmethod
+    async def _url_by_id(cls, account_id):
+        user = IGUser(account_id)
+        await user.data()
+        return user.url
+
+    async def response(self) -> list[str]:
         own_instagram = await UserDescribePlugin(self._user).instagram()
         if own_instagram:
             return own_instagram
         friends = await self._user.friends()
         await friends.data()
         users = friends.objects()
-        instagram_urls = list(filter(lambda x: isinstance(x, str), await asyncio.gather(
+        pools = await friends.pools()
+        user_pool = {}
+        for i, pool in enumerate(pools):
+            for user in pool:
+                user_pool[user] = i
+
+        ig_vk = list(filter(lambda x: isinstance(x[0], str), zip(await asyncio.gather(
             *[
                 UserDescribePlugin(user).instagram()
                 for user in users
             ],
             return_exceptions=True
-        )))
-        followers = await asyncio.gather(*[
-            self._get_followers(inst) for inst in instagram_urls
-        ])
-        followers = list(filter(bool, followers))
+        ), friends.nodes)))
+        followers = zip(await asyncio.gather(*[
+            self._get_followers(ig_vk_pair[0]) for ig_vk_pair in ig_vk
+        ]), map(lambda x: x[1], ig_vk))
+
+        followers = list(filter(lambda x: bool(x[0]), followers))
+        logger.debug('Parse %s instagram accounts to grep follower', len(followers))
         followers_butch = Counter()
-        print(len(followers))
-        for user_followers in followers:
-            if isinstance(followers, Exception):
-                logger.error('Getting followers')
-            elif isinstance(user_followers, IGCommunity):
-                followers_butch += user_followers.counter()
+        follower_score = defaultdict(set)
+        for user_followers, vk_id in followers:
+            for follower in user_followers:
+                follower_score[follower].add(user_pool.get(vk_id))
+            followers_butch += user_followers.counter()
 
+        best_followers = Counter({follower: len(clusters) for follower, clusters in follower_score.items()})
+        best = best_followers.most_common(1)[0]
+        ids = []
+        logger.debug("Most common by clusters IG candidate: %s", best)
+        if best[1] > 1:
+            ids = [best[0]]
+
+        max_count = None
         for account_id, count in followers_butch.most_common(1):
-            user = IGUser(account_id)
-            await user.data()
-            return user.url
-
-        return None
+            if not max_count or count == max_count:
+                if account_id not in ids:
+                    ids.append(account_id)
+                max_count = count
+            else:
+                break
+        logger.debug('Most common iG candidate - count: %s, len: %s', max_count, ids)
+        return list(await asyncio.gather(*[self._url_by_id(id_) for id_ in ids]))
