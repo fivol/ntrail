1bbff07b46dfc248eb8bed272e0c1b727e77e697
Boris Bondarenko <borisoffficial@gmail.com>
2021-11-24 13:31:18 +0300

    test

--- worker/tests/test_session_manager.py
+++ worker/tests/test_session_manager.py
@@ -29,8 +29,9 @@ async def test_add_session(count, postgres):
 @pytest.mark.asyncio
 async def test_using_count_sessions(postgres):
     manager = SessionManager(key_type='ig', controller=IgApiSession, requests_delay_min=10, requests_delay_max=30)
-    async with await manager.get() as api:
-        await api.get_following(8368846410, count=10, end_cursor='')
-    assert len(manager._active_sessions) == 1
+    for i in range(3):
+        async with await manager.get() as api:
+            await api.get_following(8368846410, count=10, end_cursor='')
+    assert len(manager._active_sessions) == 4
     await manager.stop()
 
