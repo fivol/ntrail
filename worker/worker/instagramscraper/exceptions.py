44f36cc160da2d942595e48978916937a3b6d613
Boris Bondarenko <borisoffficial@gmail.com>
2021-11-27 02:39:50 +0300

    Rework access factory

---
+++ worker/worker/instagramscraper/exceptions.py
@@ -0,0 +1,39 @@
+from worker.parsers.exceptions import AccessApiException
+
+
+class InstagramException(Exception):
+    """Base instagram exceptions.
+    All exception raised by this lib"""
+    code = 500
+
+    @classmethod
+    def default(cls, code=500):
+        return {
+            404: InstagramNotFoundException,
+            429: InstagramTooManyRequestsException
+        }.get(code, cls)()
+
+
+class InstagramAuthException(InstagramException):
+    """All exceptions related to account auth"""
+    code = 401
+
+
+class InstagramActionException(AccessApiException, InstagramException):
+    """Exceptions related to users requests. Such exceptions returned to the clients"""
+
+
+class InstagramLoginRedirectException(InstagramAuthException):
+    pass
+
+
+class InstagramSuspiciousActivity(InstagramAuthException):
+    code = 501
+
+
+class InstagramNotFoundException(InstagramActionException):
+    code = 404
+
+
+class InstagramTooManyRequestsException(InstagramActionException):
+    code = 429
---
+++ worker/worker/parsers/ig/session.py
@@ -0,0 +1,36 @@
+from worker.credentials.db import AccessStatus
+from worker.instagramscraper.instagram import Instagram
+from worker.instagramscraper.exceptions import *
+import logging
+
+from worker.credentials.access import AccessModel
+from worker.ctx import get_context
+from worker.session.exceptions import SessionRemove, SessionWait
+from worker.session.session_state import SessionState
+ctx = get_context()
+
+logger = logging.getLogger(__name__)
+
+
+class IgApiSession(SessionState):
+    def __init__(self, *args, **kwargs):
+        self.__ig_session = None
+        super().__init__(*args, **kwargs)
+
+    def create(self, access: AccessModel):
+        self.__ig_session = Instagram(cookie=access.data['cookie'])
+        return self.__ig_session
+
+    async def close(self):
+        pass
+
+    def handle_error(self, exc_type, exc_val, exc_tb):
+        if isinstance(exc_val, InstagramSuspiciousActivity):
+            raise SessionRemove(AccessStatus.suspicious_activity)
+        if isinstance(exc_val, InstagramTooManyRequestsException):
+            logger.error('Instagram: too many requests')
+            raise SessionWait()
+        if isinstance(exc_val, InstagramLoginRedirectException):
+            raise SessionRemove(AccessStatus.login_redirect)
+        if isinstance(exc_val, InstagramAuthException):
+            raise SessionRemove(AccessStatus.auth_error)
