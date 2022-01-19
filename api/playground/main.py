a9cedae1fddd1e062dcfb47ebb0ffdedf8423de6
Boris Bondarenko <borisoffficial@gmail.com>
2022-01-19 23:26:00 +0300

    fix vkgroup and vkgroups

--- api/playground/main.py
+++ api/playground/main.py
@@ -12,7 +12,7 @@ from tqdm import tqdm, trange
 
 from core import VKUser, VKCommunity
 from core.modules.vk.vkgroups import VKGroups
-from core import IGUser
+from core import IGUser, VKGroup
 from server.helpers.tied_value import TiedValue
 from server.plugin.register import register_plugins
 from server.plugin.plugin_manager import PluginManager
@@ -26,7 +26,10 @@ logger = logging.getLogger(__name__)
 
 async def main():
     async with Engine():
-        print(await PluginManager({'user': 'ffboris'}, input_plugins=['user'], options=['']).execute())
+        ids = [23432,5346,57,658,5,2,35,435,65,6,5,876,986,78,7,456,546,456,546,45,6456456463,2,5,234]
+        group = VKGroups(ids)
+        print(len(await group.data()), len(ids))
+        # print(await PluginManager({'user': 'ffboris'}, input_plugins=['user'], options=['']).execute())
 
 if __name__ == '__main__':
     asyncio.run(main())
